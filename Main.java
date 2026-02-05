31d2ec4586534971680878e6747004691346094b
Rex-2-ui <rexraymond35@gmail.com>
2026-02-05 17:44:41 +0000

    arrays in java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args)
+     //an array is a data structure that can hold a fixed number of values of the same type.
+   //It is used to store multiple values in a single variable, instead of declaring
+   // separate variables for each value. The values in an array are stored in
+    // contiguous memory locations and can be accessed using an index.
+
+
+    //Difference in declaration mainly applied to 2d arrays
+  { int[] month_days;// Array declaration
+    //Dynamic value assignment
+    month_days = new int[12];// Array instantiation or memory allocation
+    month_days[0] = 31; // Initializing array elements
+    System.out.println("Number of days in January: " + month_days[0]);
+    // int a, b[];
+    // int a [], b;
+    // int [] a, b;
+
+        }
+    }
